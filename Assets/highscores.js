3b3e7752735401c863f40118f53bc7e97f9d25ad
Nick Edwards <theadventuresofnickedwards@gmail.com>
2021-11-28 15:30:33 -0800

    Highscores display! Also added styling and more Rave Green text. Go Sounders

--- Assets/highscores.js
+++ Assets/highscores.js
@@ -1,14 +1,19 @@
 var scoresTable = document.getElementById("scorestable");
 var previousScores = JSON.parse(localStorage.getItem("highscores"));
 
+console.log({previousScores});
+
 function getHighscores() {
 
     for (i = 0; i < previousScores.length; i++) {
+        console.log(previousScores[i]);
         var newTableRow = document.createElement("tr");
         var name = document.createElement("td");
         var score = document.createElement("td");
         name.textContent = previousScores[i].name;
         score.textContent = previousScores[i].score;
+        newTableRow.appendChild(name);
+        newTableRow.appendChild(score);
         scoresTable.appendChild(newTableRow);
     }
 }
